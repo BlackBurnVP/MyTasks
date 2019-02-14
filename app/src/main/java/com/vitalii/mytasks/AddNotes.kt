cd7ec9d514442e9023a8dab79a211366e6a4d2d7
Vitalii Pshenychniuk <vitalii.pshenychniuk@gmail.com>
2019-02-14 15:12:47 +0100

    Added menu bar

--- app/src/main/java/com/vitalii/mytasks/AddNotes.kt
+++ app/src/main/java/com/vitalii/mytasks/AddNotes.kt
@@ -1,12 +1,25 @@
 package com.vitalii.mytasks
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add_notes.*
+import kotlinx.android.synthetic.main.task_view.*
 
 class AddNotes : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+
+        btnAdd.setOnClickListener(addNote)
+    }
+
+    val addNote = View.OnClickListener{
+        val intent = Intent(this,MainActivity::class.java)
+        intent.putExtra("title", txtTitle.text)
+        intent.putExtra("description",txtContent.text)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/vitalii/mytasks/MainActivity.kt
+++ app/src/main/java/com/vitalii/mytasks/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.vitalii.mytasks
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.task_view.view.*
 
@@ -21,12 +27,44 @@ class MainActivity : AppCompatActivity() {
 
         adapter = notesAdapter(listNotes)
         listTasks.adapter = adapter
+
     }
 
     private fun addNotes(){
         for (i in 1..10){
             listNotes.add(Notes(i,"Note no. $i","Description of note no $i"))
         }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+
+        val searchView = menu!!.findItem(R.id.app_bar_search).actionView as SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //TODO: search database
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.addNote -> {
+                val intent = Intent(this,AddNotes::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     inner class notesAdapter:BaseAdapter{
